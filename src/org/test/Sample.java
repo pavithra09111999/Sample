5e749e507179881f41e3d29ec0f24eeecafbb567
pavithra09111999 <pavifrdpavi@gmail.com>
2022-11-22 07:28:01 -0800

    B branch is updated

--- src/org/test/Sample.java
+++ src/org/test/Sample.java
@@ -5,5 +5,8 @@ public class Sample {
 	public static void main(String[] args) {
 		String s="done";
 		System.out.println(s.charAt(2));
+		 
+		String s1[]= {"java","python","selenium"};
+		System.out.println(s1[2]);
 	}
 }
